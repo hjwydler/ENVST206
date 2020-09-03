63e6382048c09d766651213a15c6b716808249a5
Hans Wydler <ginnytw@MacBook-Pro-2.local>
2020-09-02 20:49:17 -0400

    Added calculations for Activity 1

--- activity1/activity1.R
+++ activity1/activity1.R
@@ -1,4 +1,28 @@
 #Activity 1
-print("Hello R world")
+print("hello R world")
 
 2*2
+
+#if you ex out of document, everything in console is deleted (but you can replicate)
+5/4
+
+6^6 #6 to the power of six = 46656
+
+                      #Activity 1, Question 3
+
+#Calculation 1:
+x <- c(1667, 20, 12000, 1999) #creating a vector (x) w/ size of 4
+
+x*4 # =multiplying vector by 4, [6668, 80, 48000, 7996]
+
+#Calculation 2:
+
+y <- 10 #setting value of 'y' to 10
+
+y*50 #multiplying y by 50, =500
+
+#Calculation 3: 
+z <- 1.54 #setting value of z to 1.54
+
+z*2 #multiplying z * 2, =3.08
+
